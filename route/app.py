e1ec7c78930d74b8a46dddcb931be522596c4625
Howard19960920 <hower.song@gmail.com>
2018-05-05 18:30:24 +0800

    init server

---
+++ route/app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return "Hello World!"
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5001", debug=False)
+    # run the above for local testing, and the below for production environment
+    # app.run(port=5001, debug=True)
