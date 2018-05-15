c0f58ef51c38d32d667258a43f3e95d7aa7953e9
Howard19960920 <hower.song@gmail.com>
2018-05-16 00:15:10 +0800

    code reconstruct

--- route/app.py
+++ route/app.py
@@ -2,6 +2,7 @@ import os
 import string
 import pathlib
 import youtube_dl
+import subprocess
 from flask import Flask, request, make_response, send_file, send_from_directory
 from flask_cors import CORS, cross_origin
 
@@ -10,62 +11,70 @@ app = Flask(__name__)
 # app.config['JSON_AS_ASCII'] = False
 
 @app.route("/", methods = ['GET', 'POST'])
-def hello():
+def main():
   if request.method == 'POST':
-    return "Bro, don't do this...!"
+    return ''
   else:
-    return "Hello World!"
+    return 'Bonjour'
 
 @app.route("/upload", methods = ['POST'])
 def upload():
+
   file = request.files['file']
-  uid = request.form['uid'] # user_token
-  tid = request.form['tid'] # target_token
-  fid = request.form['fid'] # file_id
-  if file:
-    filename = "../storage/" + uid + "/" + tid + "/"
-    # Create the directory if not exists, do nothing while exists
-    pathlib.Path(filename).mkdir(parents=True, exist_ok=True)
-    file.save(os.path.join(filename) + str(fid).zfill(4) + ".wav")
-    return "1"
-  return "0"
+  uid = request.form['uid']
+  tid = request.form['tid']
+  fid = request.form['fid']
+  if not file:
+    return 'FileNotRecievedError'
+  
+  filename = "../storage/" + uid + "/" + tid + "/src/"
+  # Create the directory if not exists, do nothing while exists
+  pathlib.Path(filename).mkdir(parents = True, exist_ok = True)
+  file.save(os.path.join(filename) + str(fid).zfill(4) + ".wav")
+  return 'SuccessUploadFile'
+
 
 @app.route("/convert", methods = ['POST'])
 def convert():
-  file = request.files['file']
-  uid = request.form['uid'] # user_token
-  tid = request.form['tid'] # target_token
-  fid = 'tobeconvert.wav'
-  if file:
-    filename = "../storage/" + uid + "/"
-    # Create the directory if not exists, do nothing while exists
-    pathlib.Path(filename).mkdir(parents=True, exist_ok=True)
-    file.save(os.path.join(filename) + fid)
-    return filename + fid
-  return "0"
 
-"""
-# Directly return byte data, put it into memory
-def convert():
   file = request.files['file']
-  response = make_response(file.read())
-  response.headers['Content-Type'] = 'audio/wav'
-  return response
-"""
+  uid = request.form['uid']
+  tid = request.form['tid']
+  if not file:
+    return 'FileNotRecievedError'
+  
+  filepath = "../storage/" + uid
+  sprocketPath = '../../sprocket/example/onServer.py'
+  ftemp = '/tempaudio.wav'
+  fconvert = '/tobeconvert.wav'
+  freturn = '/afterconvert.wav'
+
+  pathlib.Path(filepath).mkdir(parents = True, exist_ok = True)
+  file.save(os.path.join(filepath) + ftemp)
+  # If overwrites to 'tobeconvert.wav' will cause unknown error
+  subprocess.call(['ffmpeg', '-y', '-i', filepath + ftemp, filepath + fconvert])
+  cmd = ['python3', sprocketPath, uid, tid]
+  subprocess.Popen(cmd).wait()
+  return filepath + freturn
 
 @app.route("/download", methods = ['GET'])
 def download():
+
   filename = request.args.get("filename")
+  # return byte data ?
+  # response = make_response(file.read())
   response = make_response(send_file(filename))
   response.headers['Content-Type'] = 'audio/wav'
-  #response.headers['Content-Disposition'] = "attachment"
+  # response.headers['Content-Disposition'] = "attachment"
   return response
+
 
 @app.route('/<path:dummy>', methods = ['POST', 'GET'])
 def fallback(dummy):
-  return "Bro, don't do this...!"
+  return 'Undefined Request..., bro, stop doing this...'
 
 if __name__ == "__main__":
+  # for production environment
   app.run(host="0.0.0.0", port="5001", debug=False)
-  # run the above for local testing, and the below for production environment
+  # for local testing
   # app.run(port=5001, debug=True)
