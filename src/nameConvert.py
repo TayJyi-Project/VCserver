85361c8e168a3f2f16d1aaa1c8967a536b2161fd
Howard19960920 <hower.song@gmail.com>
2018-05-23 01:11:05 +0800

    fix re

--- src/nameConvert.py
+++ src/nameConvert.py
@@ -2,6 +2,7 @@ import os
 import re
 items = os.listdir()
 for item in items:
-  if re.search('.*\d+.*\.wav', item):
+  if re.search('.*\d+.*\.\w+', item):
     num = int(re.findall('\d+', item)[0])
-    os.rename(item, 'chunk{0}.wav'.format(str(num).zfill(3)))
+    ext = re.findall('\.[a-zA-Z]+', item)[0]
+    os.rename(item, 'chunk{0}{1}'.format(str(num).zfill(3), ext))
